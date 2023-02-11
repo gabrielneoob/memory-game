a1580cf701c7ed2caf37c854aba617edc2f41460
Gabriel Neoob <gabrielngc@gmail.com>
2023-02-10 23:37:46 -0300

    button component styled

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,20 +1,24 @@
 import React, { ReactNode } from 'react'
-import { ButtonWrapp } from './style'
+import { Container, Icon, IconArea, Label} from './style'
 
 type ButtonProps = {
-  children: ReactNode;
-  onClick?: () => void;
+  children?: ReactNode;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+  label: string;
+  icon?: any;
 }
 
-const Button = ({children, onClick, ...props}: ButtonProps) => {
+const Button = ({children, onClick, label, icon}: ButtonProps) => {
   return (
-    <ButtonWrapp 
-    type='button'
-    onClick={onClick}
-    {...props}
-    >
-      {children}
-    </ButtonWrapp>
+    <Container onClick={onClick}>
+      {icon && 
+        <IconArea>
+          <Icon src={icon} alt=''/> 
+        </IconArea>
+      }      
+      <Label>{label}</Label>
+    </Container>
+    
   )
 }
 
